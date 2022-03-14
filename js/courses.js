a7665cb9f7d36f7c2ee79d7c17d6b9d73fe39ab7
nahiduzzaman2021 <akash.nahid.27@gmail.com>
2022-03-14 14:45:29 +0600

    courses filter

--- js/courses.js
+++ js/courses.js
@@ -1,29 +1,43 @@
-//selecting all required elements
-const filterItem = document.querySelector(".items");
-const filterGallery = document.querySelectorAll(".gallery .gallery-box");
-console.log(filterItem, filterGallery)
+filterSelection("all")
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("column-section");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
 
-window.onload = () => { //after window loaded
-    filterItem.onclick = (selectedItem) => { //if user click on filterItem div
-        if (selectedItem.target.classList.contains("item")) { //if user selected item has .item class
-            filterItem.querySelector(".active").classList.remove("active"); //remove the active class which is in first item
-            selectedItem.target.classList.add("active"); //add that active class on user selected item
-            let filterName = selectedItem.target.getAttribute("data-name"); //getting data-name value of user selected item and store in a filtername variable
-            filterGallery.forEach((galleryBox) => {
-                let filterBoxes = galleryBox.getAttribute("data-name"); //getting galleryBox data-name value
-                //if user selected item data-name value is equal to galleryBoxs data-name value
-                //or user selected item data-name value is equal to "all"
-                if ((filterBoxes == filterName) || (filterName == "all")) {
-                    galleryBox.classList.remove("hide"); //first remove the hide class from the galleryBox
-                    galleryBox.classList.add("show"); //add show class in galleryBox
-                } else {
-                    galleryBox.classList.add("hide"); //add hide class in galleryBox
-                    galleryBox.classList.remove("show"); //remove show class from the galleryBox
-                }
-            });
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
+    }
+}
+
+function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
         }
     }
-    for (let i = 0; i < filterGallery.length; i++) {
-        filterGallery[i].setAttribute("onclick", "preview(this)"); //adding onclick attribute in all available galleryBoxs
-    }
+    element.className = arr1.join(" ");
+}
+
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("course-nav-btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
 }
